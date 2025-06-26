96dbb00ee1a83337ca2982afe8a7598faacb683d
Utkarsh Pandey <utkarshji14@gmail.com>
2025-06-26 17:55:33 +0530

    Auto backup on 26-06-2025 at 17:55:32.13

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest
 from .models import User,PendingUser
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 from django.utils.crypto import get_random_string
+from datetime import datetime,timezone
+from common.tasks import send_email
 
 
 # Create your views here.
@@ -18,8 +21,22 @@ def register(request: HttpRequest):
             return redirect("register")
         else:
             verification_code=get_random_string(10)
-            PendingUser.objects.create(
-                email=cleaned_email
+            PendingUser.objects.update_or_create(
+                email=cleaned_email,
+                defaults={
+                    "password": make_password(password),
+                    "verification_code": verification_code,
+                    "created_at": datetime.now(timezone.utc)
+                }
             )
+            send_email(
+                "Verify Your Account",
+                [cleaned_email],
+                "email/email_verification_template.html",
+                context={'code': verification_code}
+            )
+            messages.success(request,f"Verification code sent to {cleaned_email}")
+            return render(request,"verify_account.html")
+        
     else:
         return render(request,"register.html")
